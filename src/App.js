e44f1e9eeb879bc874b3bd46672c45e208ca2e62
qaisw96 <qaisw14@gmail.com>
2021-06-19 19:09:33 +0300

    done

--- src/App.js
+++ src/App.js
@@ -14,12 +14,16 @@ class App extends React.Component {
      headers : {},
      results: {},
      show: false,
+     load: false,
      history: localStorage.getItem('history')? JSON.parse(localStorage.getItem('history')) : [],
-     query: {}
+     query: {
+       url: null
+     }
     }
   }
 
   getFormData = async (Form, headers, results) => {
+    console.log('from app before storing LS', Form);
     console.log(Form, headers, results);
      this.setState({headers, results})
      let query = {method: Form.method, url: Form.url}
@@ -29,18 +33,22 @@ class App extends React.Component {
   
   handleShow = (check) => {
     this.setState({show: check})
-  } 
-
-  getQueryFromHistory = async (method, url) => {
-    await this.setState({query: {method, url}})
+  }
+  
+  handleLoading = (load) => {
+    this.setState({load: load})
   }
 
+  getQueryFromHistory =  (method, url) => {
+     this.setState({query: {method, url}})
+  }
+  
 
   render() {
     return (
       <div className={this.show ? 'hidden': 'visible'} >
         <Header />
-        <Form getMethod={this.getMethod} getFormData={this.getFormData} handleShow= {this.handleShow} query={this.state.query} />
+        <Form getMethod={this.getMethod} getFormData={this.getFormData} handleShow= {this.handleShow} query={this.state.query} handleLoading={this.handleLoading} />
         <div className="display" >
           <History queries={this.state.history}  getQuery={this.getQueryFromHistory} />
           <Results result={this.state}/>
--- src/components/History.js
+++ src/components/History.js
@@ -2,11 +2,14 @@ import '../css/History.scss'
 
 const History = ({queries, getQuery}) => {
     return (
+        <>
         <div className="queries">
+        <h3>Previous Queries : </h3>
             {queries.map((item, idx) => {
-                return <p key={idx} onClick={() => getQuery(item.method, item.url)} >{item.method} ======= {item.url} </p>
+                return <p key={idx} onClick={() => getQuery(item.method, item.url)} > <span>{item.method} >></span>  {item.url} </p>
             })}
         </div>
+        </>
     )
 }
 
